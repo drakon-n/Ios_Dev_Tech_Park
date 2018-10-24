703aec0c1ae53908e563097d1677f69bf95a42f3
Drakon_n <ttviimail@mail.ru>
2018-10-24 15:50:35 +0300

    Base commit
    Project base and new laucnh screen

---
+++ Moscow_Places/Moscow_Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moscow_Places
+//
+//  Created by Влад on 24/10/2018.
+//  Copyright © 2018 Влад. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
